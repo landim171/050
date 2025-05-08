1af744f23a95e2020d227baa1dae9598a56f08f2
EA - Daniel Landim De Araujo Silva <daniel_landim@educadventista.org>
2025-05-08 11:33:45 -0300

    trabalho em aula

---
+++ main.java
@@ -0,0 +1,79 @@
+/*
+exemplo 1
+function mensagem () {
+    alert("seja muito bem-vindo(a)!")
+    console.log("mais uma vez, seja bem-vindo(a)")
+}
+
+mensagem()
+*/
+
+/*
+exemplo 2
+function somar (valor1, valor2) {
+    alert(valor1 + valor2)
+}
+
+somar(30, 40)
+*/
+
+
+/*
+exemplo 3
+
+function mensagemPessoal(nome, cargo, anosTrabalhados) {
+    alert(`seja bem-vindo(a) ${cargo} ${nome}. Estamos felizes por ter voçe
+        trabalhando conosco há ${anosTrabalhados} anos.`)
+}
+
+mensagemPessoal("Lucas", "gerente", 10)
+mensagemPessoal("Irineu", "voçe não sabe, nem eu", 50)
+*/
+
+
+/*
+exemplo 4
+function somarEDevolver(numero1, numero2, numero3) {
+    let resultado = numero1 + numero2 + numero3
+    return resultado
+}
+
+let somar1 = somarEDevolver(10, 11, 12)
+let somar2 = somarEDevolver(30, 40, 50)
+
+alert(somar1)
+alert(somar2)
+*/
+
+/*
+exemplo 5
+function parOuImpar(numero) {
+    if (numero % 2 === 0) {
+        return "par"
+    }
+     
+         return "ímpar"
+    }
+
+let resultado1 = parOuImpar(10)
+let resultado2 = parOuImpar(15)
+
+alert(`o número 10 é ${resultado1}`)
+alert(`o número 15 é ${resultado2}`)
+*/
+
+/*
+exemplo 6
+
+document.querySelector("button").addEventListener("click", () => {
+    alert("clicou no botão")
+})
+*/
+
+/*
+exemplo 7
+let somar = ( valor1 , valor2) => valor1 + valor2
+
+let resultadoSoma = somar(20,30)
+alert(resultadoSoma)
+*/
